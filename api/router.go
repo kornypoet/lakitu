9a05f160c54c98653121fa8d9b3897affc00be5e
Travis Dempsey <dempsey.travis@gmail.com>
2022-02-10 19:20:21 -0600

    manage_file route

--- api/router.go
+++ api/router.go
@@ -6,6 +6,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Payload struct {
+	Action string `json:"action" binding:"required"`
+}
+
 func Router(debug bool) *gin.Engine {
 	gin.DisableConsoleColor()
 
@@ -18,10 +22,20 @@ func Router(debug bool) *gin.Engine {
 	v1 := router.Group("/v1")
 
 	v1.GET("/ping", Ping)
+	v1.POST("/manage_file", ManageFile)
 
 	return router
 }
 
 func Ping(c *gin.Context) {
 	c.String(http.StatusOK, "pong")
+}
+
+func ManageFile(c *gin.Context) {
+	var payload Payload
+	if err := c.ShouldBindJSON(&payload); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "failure", "err": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
 }
